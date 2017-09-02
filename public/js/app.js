8f9d010f01808875a894ac3b476f8a4a97a097d0
Wasif Zaman <teatocode@gmail.com>
2017-09-02 06:11:14 -0400

    wired up user form and list

--- public/js/app.js
+++ public/js/app.js
@@ -1,74 +1,142 @@
 $(function() {
 
-  $.get('/users')
-    .then(obj=> {
-      genUserPanel({
-        users: obj.users,
-        offices: obj.offices,
-        parent: '#users'
-      })
+  let users = [
+    {
+      name: 'John Smithy',
+      id: 1,
+      officeId: 1
+    },
+    {
+      name: 'Pocahontas',
+      id: 2,
+      officeId: 2
+    },
+    {
+      name: 'Willow',
+      id: 3,
+      officeId: 2
+    }
+  ],
+  offices = [
+    {
+      name: 'SS India',
+      id: 1,
+      lat: 40,
+      lang: 70
+    },
+    {
+      name: 'Village',
+      id: 2,
+      lat: 80,
+      lang: -90
+    }
+  ]
+
+
+
+
+
+  const genUser = (config)=> {
+    let user = `
+      <li>
+        ${config.user.name}
+        <select>
+          <option>---</option>
+          ${ config.offices.reduce((aggr, office)=> {
+              if (config.user.officeId && office.id == config.user.officeId) return aggr += `
+                <option selected="selected">${office.name}</option>
+              `;
+              return aggr += `
+                <option>${office.name}</option>
+              `;
+            }, '')}
+        </select>
+        <br/>
+        <button>Delete</button>
+      </li>
+    `;
+
+    let $user = $(user);
+    $user.find('select').on('change', function() {
+      // TODO: do ajax stuff
+      console.log('did stuff');
     })
 
-  $.get('/offices')
-    .then(offices=> {
-      genOfficePanel({
-        offices,
-        parent: '#offices'
-      })
-    })
+    return $user;
+  }
 
-  const genOfficePanel = (config)=> {
-    let template = `
+  const genOffice = (config)=> {
+
+  }
+
+  const genUserForm = (config)=> {
+    /* NOTE: users offices global
+      pushes to userlist dom
+      pushes to users
+    */
+    let form = `
       <div>
-        ${ config.offices.reduce((aggr, office)=> {
-            return aggr += `
-              <li>
-                ${office.name}
-                ${office.lat}
-                ${office.lang}
-                <br/>
-                Users: ${office.users.length}
-              </li>
-              <br/>
-            `;
-          }, '') }
+        New user:
+        <input type="text"/>
+        <button>Add!</button>
       </div>
     `;
 
-    let $html = $(template);
+    let $form = $(form),
+      $input = $form.find('input');
 
-    $(config.parent).append($html);
+    $form.on('click', 'button', function() {
+      // TODO: do ajax stuff, then get user
+      let user = { name: $input.val() }; //fake
+      $newUser = genUser({
+        user,
+        offices: config.offices
+      })
+
+      config.users.push(user)
+      $(config.userlist).append($newUser);
+    })
+
+    $(config.parent).append($form);
   }
 
-  const genUserPanel = (config)=> {
-    // Office: ${user.office.name}
-    let template = `
-      <div>
-        ${ config.users.reduce((aggr, user)=> {
-            return aggr += `
-              <li>
-                ${user.name} <br/>
-                <select>
-                  <option>---</option>
-                  ${ config.offices.reduce((aggr, office)=> {
-                      if (user.office && office.id == user.office.id) return aggr += `
-                        <option selected="selected">${office.name}</option>
-                      `;
-                      return aggr += `
-                        <option>${office.name}</option>
-                      `;
-                    }, '')}
-                </select>
-              </li>
-              <br/>
-            `;
-          }, '') }
-      </div>
+  const genOfficeForm = (config)=> {
+
+  }
+
+  const genUserList = (config)=> {
+    let userlist = `
+      <ul></ul>
     `;
 
-    let $html = $(template);
+    let $userlist = $(userlist);
 
-    $(config.parent).append($html);
+    config.users.forEach(user=> {
+      $user = genUser({
+        user: user,
+        offices: config.offices
+      })
+
+      $userlist.append($user);
+    })
+
+    $(config.parent).append($userlist);
+    return $userlist;
   }
 
+  const genOfficeList = (config)=> {
+
+  }
+
+  let $userlist = genUserList({
+    parent: '#user-list',
+    users,
+    offices
+  }),
+  $userform = genUserForm({
+    parent: '#user-form',
+    userlist: $userlist,
+    users,
+    offices
+  })
 })
