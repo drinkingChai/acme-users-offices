bc347728dc0a4a835b775654c5f38134c9a3cd0a
Wasif Zaman <teatocode@gmail.com>
2017-09-03 10:01:43 -0400

    wrote plan

--- public/js/app.js
+++ public/js/app.js
@@ -7,25 +7,32 @@ $(function() {
       return $.get('/offices')
     })
     .then(offices=> {
-      drawUserForm({
-        id: '#user-form',
-        users,
-        offices
-      })
 
-      drawOfficeForm({
-        id: '#office-form',
-        users,
-        offices
-      })
+      var dataBank = {
+        users, offices
+      }
 
-      drawUsers({
-        users,
-        offices
-      });
-      drawOffices({
-        users,
-        offices
-      });
+      /*
+        NOTE concept NOTE
+
+      create an item, and give that item an "update" function
+      create another item, and within it's targets: [] array, add other items
+      on an event click, it will run "update" on all items in target targets.forEach(t=> t.update(newData));
+
+      NOTE: the dataBank "global" will be passed around between items to keep them up to date
+
+      component layout:
+        update(dataBank)
+
+        OfficeList (dataBank)
+        UserList (dataBank)
+        OfficeForm (dataBank)
+          targets: [ OfficeList, UserList ]
+        UserForm (dataBank)
+          targets: [ UserList ]
+
+      */
+
+
     })
 })
